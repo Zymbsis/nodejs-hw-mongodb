4f325c0c5043d1da691a2392ef0628567edcc832
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-05-22 23:30:39 +0200

    update

--- src/server.js
+++ src/server.js
@@ -32,23 +32,26 @@ export function setupServer() {
       data: students,
     });
   });
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+      if (!contact) {
+        return res.status(404).json({
+          status: 'fail',
+          message: `Contact with id ${contactId} not found!`,
+        });
+      }
 
-    res.status(200).json({
-      status: 'success',
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('/contacts/:contactId', async (error, req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-    next();
+      res.status(200).json({
+        status: 'success',
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      next(error);
+    }
   });
 
   app.use('*', (req, res) => {
