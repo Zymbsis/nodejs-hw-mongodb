de2df10189be8813d6bc261dd8bb76f096f08c18
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-05-22 21:57:44 +0200

    .

---
+++ src/db/contactModel.js
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+        },
+      },
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Contact = model('Contact', contactSchema);
---
+++ src/server.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './env.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = (await initMongoConnection()) || Number(env('PORT', '3000'));
+
+export function setupServer() {
+  const app = express();
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const students = await getAllContacts();
+    res.status(200).json({
+      status: 'success',
+      message: 'Successfully found contacts!',
+      data: students,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    res.status(200).json({
+      status: 'success',
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (error, req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
