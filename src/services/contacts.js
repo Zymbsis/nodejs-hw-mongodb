a4252b8bf681e046cf3f1bb6acd88a4a8b544e68
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-06-18 21:19:09 +0200

    min fix

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -39,7 +39,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactById = async (contactId, userId) => {
-  const contact = await Contact.findById(contactId).findOne({ userId: userId });
+  const contact = await Contact.findOne({ _id: contactId, userId: userId });
   return contact;
 };
 
